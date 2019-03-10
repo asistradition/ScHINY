8e9f0cab1e8a220578747022c3b49af2f07bbe32
CJackson <christopher@cajackson.org>
2019-03-10 12:01:02 -0400

    Padded labels

--- figures/figure_2_ridgeline.R
+++ figures/figure_2_ridgeline.R
@@ -10,15 +10,25 @@ plot.figure.2.ridgeline <- function(shiny.data, gene.vec, input) {
   ridgeline.medians <- dplyr::group_by(ridgeline.data, Gene, Condition, Genotype_Group)
   ridgeline.medians <- dplyr::summarize(ridgeline.medians, gene_median=median(Count))
   
+  # Make a faceted ridgeline plot for each gene
   plt.list <- lapply(as.list(gene.vec), function(x) {make.ridgeline.plt(ridgeline.data, ridgeline.medians, x) + labs(title=yeast.systemic.to.common(x))})
+  
+  # Pull the first plot off and hack it into just being labels
   plt.labels <- plt.list[[1]] + 
     scale_x_continuous(limits = c(0, 0), breaks = c(0)) + 
     theme_void() +
     theme(plot.title = element_blank(), axis.text.y = element_text(size=LABEL.FONT.SIZE, vjust=0, hjust=1, face="bold", color="black"),
           axis.title.x = element_blank(), legend.position = 'none', plot.margin = unit(c(0, 0, 0, 0), "cm"))
   plt.labels$layers[[2]] <- NULL
+  
+  # Stack up the label plots with some whitespace on the top and bottom
+  # Stack up the ridgeline facet plots left to right with the labels on the left
+  plot.stack <- plot_grid(plot_grid(ggdraw(), plt.labels, ggdraw(), ncol = 1, align="hv", rel_heights = c(.025, 1, .05)), 
+                          plot_grid(plotlist = plt.list, ncol = length(plt.list), align="hv"), 
+                          ncol=2, rel_widths = c(.1, 1), align="hv")
+  
+  # Stack up all the plots with an x-axis title on the bottom
   x.axis.title <- ggdraw() + draw_label("Transcript Counts", fontface='bold')
-  plot.stack <- plot_grid(plt.labels, plot_grid(plotlist = plt.list, ncol = length(plt.list), align="h"), ncol=2, rel_widths = c(.1, 1), align="hv")
   return(plot_grid(plot.stack, x.axis.title, ncol=1, rel_heights = c(1,0.05)))
 }
 
@@ -29,8 +39,8 @@ make.ridgeline.plt <- function(ridge.data, median.data, gene.name) {
   ridge.data <- ridge.data[ridge.data$Gene == gene.name, ]
   x.max <- max(quantile(ridge.data$Count, probs = 0.99)[[1]], 1)
   ggplot(ridge.data, aes(x=Count, y=Condition)) +
-    stat_binline(aes(height=..ndensity.., fill=factor(Condition)), binwidth=1, center=0, draw_baseline = FALSE, scale = 0.9) + 
-    geom_point(data=median.data[median.data$Gene == gene.name, ], aes(x=gene_median, y=Condition)) +
+    stat_binline(aes(height=..ndensity.., fill=factor(Condition)), binwidth=1, center=0, draw_baseline = FALSE, scale = 0.9, na.rm = TRUE) + 
+    geom_point(data=median.data[median.data$Gene == gene.name, ], aes(x=gene_median, y=Condition), na.rm = TRUE) +
     theme_ridges(grid = FALSE) +
     scale_y_discrete(limits=CONDITION.LEVELS, labels=CONDITION.LABELS) +
     scale_x_continuous(limits = c(-0.5, x.max), breaks = function(x) {ceiling(scales::pretty_breaks(n=3)(x))}) +
